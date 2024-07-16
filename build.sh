819e733a077c462a9c535d7b7b7cd84d9cecb745
root <admin@example.com>
2024-07-17 01:37:40 +0800

    Use llvm clang

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clang-18 -O0 -nostdlib -c -o main.o main.c
-as -nostdlib -r -o hello.o hello.asm
+clang-18 -nostdlib -c -o main.o main.c
+clang-18 -nostdlib -c -o hello.o hello.asm
 clang-18 -o main main.o hello.o
