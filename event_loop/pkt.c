0d0b315072ed320cd8ff805d5c5a35537963f542
root <admin@example.com>
2024-07-24 22:55:23 +0800

    save

--- event_loop/pkt.c
+++ event_loop/pkt.c
@@ -13,42 +13,63 @@ struct pkt_impl {
   int receiver_length;
   char *receiver;
 
-  int body_length;
   char *body;
+  int body_length;
+  int body_capacity;
 
   void *(*alloc)(const int, void *);
   void *alloca_closure;
+
+  void (*deleter)(void *, void *);
+  void *deleter_closure;
 };
+
+// align x to multiples of 2^m
+int align_to(int x, int m) {
+  int mask = (((int)1) << m);
+  int rem = x % m;
+  int result = x - rem;
+  return rem == 0 ? x - rem : x - rem + mask;
+}
+
+#define DEFAULT_ALIGN_M 4
+int align_default(int x) { return align_to(x, DEFAULT_ALIGN_M); }
 
 void *pkt_default_alloca(const int size, void *closure) { return malloc(size); }
 
 void pkt_default_deleter(void *p, void *closure) { free(p); }
 
-struct pkt_impl *pkt_create(int type, void *(*alloca)(const int, void *),
-                            void *alloca_closure) {
+struct pkt_impl *pkt_create(int type,
+                            void *(*alloca)(const int size, void *closure),
+                            void *alloca_closure,
+                            void (*deleter)(void *obj, void *payload),
+                            void *deleter_closure) {
   struct pkt_impl *pkt;
   void *(*used_alloc)(const int, void *) =
       alloca == NULL ? pkt_default_alloca : alloca;
   pkt = used_alloc(sizeof(struct pkt_impl), alloca_closure);
   pkt->alloc = used_alloc;
   pkt->alloca_closure = alloca_closure;
-  pkt->body_length = 0;
   pkt->sender_length = 0;
   pkt->receiver_length = 0;
   pkt->body = NULL;
+  pkt->body_length = 0;
+  pkt->body_capacity = 0;
   pkt->receiver = NULL;
   pkt->sender = NULL;
+  pkt->deleter = deleter;
+  if (!pkt->deleter) {
+    pkt->deleter = pkt_default_deleter;
+  }
+  pkt->deleter_closure = deleter_closure;
   return pkt;
 }
 
-void pkt_free(struct pkt_impl **p, void (*deleter)(void *p, void *closure),
-              void *deleter_closure) {
-  void (*used_deleter)(void *p, void *closure) = deleter;
-  if (used_deleter == NULL) {
-    used_deleter = pkt_default_deleter;
-  }
-
+void pkt_free(struct pkt_impl **p) {
   struct pkt_impl *pkt = *p;
+  void (*used_deleter)(void *p, void *closure) = pkt->deleter;
+  void *deleter_closure = pkt->deleter_closure;
+
   if (pkt->sender) {
     used_deleter(pkt->sender, deleter_closure);
   }
@@ -66,23 +87,28 @@ void pkt_free(struct pkt_impl **p, void (*deleter)(void *p, void *closure),
 }
 
 int pkt_set_sender(struct pkt_impl *p, char *buf, int length) {
-  if (!p->sender) {
-    p->sender = p->alloc(length, p->alloca_closure);
-    if (!p->sender) {
-      return ErrAllocaFailed;
-    }
+  if (p->sender) {
+    p->deleter(p->sender, p->deleter_closure);
   }
+
+  p->sender = p->alloc(length, p->alloca_closure);
+  if (!p->sender) {
+    return ErrAllocaFailed;
+  }
+
   p->sender_length = length;
   memcpy(p->sender, buf, length);
   return 0;
 }
 
 int pkt_set_receiver(struct pkt_impl *p, char *buf, int length) {
+  if (p->receiver) {
+    p->deleter(p->receiver, p->deleter_closure);
+  }
+
+  p->receiver = p->alloc(length, p->alloca_closure);
   if (!p->receiver) {
-    p->receiver = p->alloc(length, p->alloca_closure);
-    if (!p->receiver) {
-      return ErrAllocaFailed;
-    }
+    return ErrAllocaFailed;
   }
 
   p->receiver_length = length;
@@ -129,5 +155,48 @@ int pkt_header_get_value(struct pkt_impl *p, int key_idx, char *buf,
       return ErrNonSupportedField;
   }
 
+  return 0;
+}
+
+int pkt_body_buf_upscale_on_demand(struct pkt_impl *p, int length_addend) {
+  if (p->body_capacity < p->body_length + length_addend) {
+    int new_capacity = align_default(p->body_length + length_addend);
+    int new_body_buf = p->alloc(new_capacity, p->alloca_closure);
+    if (!new_body_buf) {
+      return ErrAllocaFailed;
+    }
+    if (p->body_length > 0 && p->body != NULL) {
+      memcpy(new_body_buf, p->body, p->body_length);
+      p->deleter(p->body, p->deleter_closure);
+    }
+    p->body = new_body_buf;
+    p->body_capacity = new_capacity;
+  }
+  return 0;
+}
+
+int pkt_body_send_chunk(struct pkt_impl *p, char *buf, int length) {
+  if (p->body_length + length > MAX_BODY_SIZE) {
+    return ErrBodyTooLarge;
+  }
+
+  int status = pkt_body_buf_upscale_on_demand(p, length);
+  if (status != 0) {
+    return status;
+  }
+
+  memcpy(&p->body[p->body_length], buf, length);
+  return 0;
+}
+
+int pkt_body_receive_chunk(struct pkt_impl *p, char *buf, int length,
+                           int *chunk_size, int offset) {
+  int remain_length = p->body_length - offset;
+  *chunk_size = length > remain_length ? remain_length : length;
+  if (*chunk_size == 0) {
+    return 0;
+  }
+  char *src = (void *)(p->body);
+  memcpy(buf, &src[offset], *chunk_size);
   return 0;
 }
--- event_loop/pkt.h
+++ event_loop/pkt.h
@@ -19,6 +19,7 @@ enum PktHeaderField {
 };
 
 #define MAX_HEADER_VALUE_SIZE ((0x1UL) << 10)
+#define MAX_BODY_SIZE ((0x1UL) << 24)
 
 enum ErrorReason {
   // 内存分配失败
@@ -31,7 +32,12 @@ enum ErrorReason {
   ErrSizeTooLarge = 3,
 
   // Buffer太小。有可能不能一次性放下要读取的字段的值，造成内存越界读取或者数据截断）。
-  ErrTooSmallBuffer = 4
+  ErrTooSmallBuffer = 4,
+
+  // 超过了 body size 极限大小，请减小写入的 chunk
+  // 大小（或者考虑将消息拆分为多个 packet，todo：packet reassemble 待实现）
+  // body 最大不能超过 MAX_BODY_SIZE
+  ErrBodyTooLarge = 5
 };
 
 // 创建一个全新的 packet，使用该函数创建的 packet 用完后要通过 pkt_free
@@ -42,8 +48,9 @@ enum ErrorReason {
 // 本身以及子资源的资源分配。
 // 当 allocator 为 NULL（或默认 allocator 时），allocator_closure 不会被提领。
 // type 的取值详见 enum PktType.
-pkt *pkt_create(int type, void *(*allocator)(const int, void *closure),
-                void *allocator_closure);
+pkt *pkt_create(int type, void *(*alloca)(const int size, void *closure),
+                void *alloca_closure, void (*deleter)(void *obj, void *payload),
+                void *deleter_closure);
 
 // 对 p 指向的 pkt 指针指向的 pkt 内存区域进行释放，释放后对 p 指向的 pkt 指针置
 // NULL。
@@ -51,8 +58,7 @@ pkt *pkt_create(int type, void *(*allocator)(const int, void *closure),
 // allocator，那么，需要调用者提供匹配的 deleter。使用 NULL 作为 deleter 如果在
 // pkt_create 中指定了使用默认 allocator。
 // 当 deleter 为 NULL（或默认 deleter 时），deleter_closure 不会被提领。
-void pkt_free(pkt **p, void (*deleter)(void *p, void *closure),
-              void *deleter_closure);
+void pkt_free(pkt **p);
 
 // 对 key_idx 指代的 header 字段进行设置，值为 (buf, length) 指代的 blob。
 // key_idx 的取值详见 enum PktHeaderField。
@@ -67,12 +73,13 @@ int pkt_header_set_value(pkt *p, int key_idx, char *buf, int length);
 int pkt_header_get_value(pkt *p, int key_idx, char *buf, int length);
 
 // 对 packet 的 body 进行写入
-void pkt_body_send_chunk(pkt *p, char *buf, int length);
+int pkt_body_send_chunk(pkt *p, char *buf, int length);
 
 // 对 packet 的 body 进行读取，出错时返回负值，读取完毕返回
 // 0，其它情况下返回得到的 chunk 的实际大小。每次读取不超过 length
 // 字节。实际长度被写入 chunk_size。
-int pkt_body_receive_chunk(pkt *p, char *buf, int length, int *chunk_size);
+int pkt_body_receive_chunk(pkt *p, char *buf, int length, int *chunk_size,
+                           int offset);
 
 // 把一个 packet 发送到 serialize context，出错时返回非 0 值。
 int serialze_ctx_send_pkt(serialize_ctx *s_ctx, pkt *p);
