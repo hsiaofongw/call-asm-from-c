3b0e6d66d9d8aea69b90804e09a108912d5fed02
root <admin@example.com>
2024-07-30 23:35:36 +0800

    save

---
+++ event_loop/queue.c
@@ -0,0 +1,54 @@
+#include "queue.h"
+
+#include <stdlib.h>
+
+#include "ringbuf.h"
+
+struct queue_impl {
+  ringbuf *data;
+};
+
+queue *queue_create(int max_len) {
+  struct queue_impl *q_ptr = malloc(sizeof(struct queue_impl));
+  q_ptr->data = ringbuf_create(max_len * sizeof(void *));
+  return q_ptr;
+}
+
+void queue_free(queue **q) {
+  if (!q) {
+    return;
+  }
+
+  queue *q_ptr = *q;
+  if (!q_ptr) {
+    return;
+  }
+
+  ringbuf_free(q_ptr->data);
+  free(q_ptr);
+  *q = NULL;
+}
+
+int queue_is_fullfilled(queue *q) {
+  return ringbuf_get_remaining_capacity(q->data) == 0;
+}
+
+int queue_has_space(queue *q) {
+  return ringbuf_get_remaining_capacity(q->data) > 0;
+}
+
+void queue_enqueue(queue *q, void *item) {
+  ringbuf_send_chunk(q->data, (char *)&item, sizeof(void *));
+}
+
+void queue_dequeue(queue *q, void **output) {
+  ringbuf_receive_chunk(*output, sizeof(void *), q->data);
+}
+
+int queue_get_size(queue *q) {
+  return ringbuf_get_size(q->data) / sizeof(void *);
+}
+
+int queue_get_capacity(queue *q) {
+  return ringbuf_get_capacity(q->data) / sizeof(void *);
+}
---
+++ event_loop/queue.h
@@ -0,0 +1,31 @@
+#ifndef MY_QUEUE
+#define MY_QUEUE
+
+struct queue_impl;
+typedef struct queue_impl queue;
+
+// 创建一个 queue 对象
+queue *queue_create(int max_len);
+
+// 释放一个 queue 对象
+void queue_free(queue **);
+
+// 判断队列是否已满
+int queue_is_fullfilled(queue *);
+
+// 判断队列是否未满
+int queue_has_space(queue *q);
+
+// 把一个元素追加至队尾
+void queue_enqueue(queue *q, void *item);
+
+// 把一个元素从队列头部取出
+void queue_dequeue(queue *q, void **output);
+
+// 获取队列当前元素数量
+int queue_get_size(queue *q);
+
+// 获取队列容量
+int queue_get_capacity(queue *q);
+
+#endif
